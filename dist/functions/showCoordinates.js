42d6a017dd49cf54e72801ed6df99e31bbd7f7e3
Aditya <105863819+Clyders@users.noreply.github.com>
2024-08-23 19:01:15 +0530

    Update showCoordinates.js

--- dist/functions/showCoordinates.js
+++ dist/functions/showCoordinates.js
@@ -1,7 +1,8 @@
 "use strict";
 const { NativeFunction, ArgType } = require("@tryforge/forgescript");
 const { ForgeCanvas } = require("..");
-const { CanvasBuilder } = require("./classes/builder");
+const { CanvasBuilder } = require("../classes");
+const { AttachmentBuilder } = require("discord.js");
 
 module.exports = new NativeFunction({
     name: "$showCoordinates",
@@ -13,26 +14,31 @@ module.exports = new NativeFunction({
     args: [
         {
             name: "canvas",
-            description: "The name of the canvas to draw X and Y coordinates on.",
+            description: "The name of the canvas where the coordinates will be shown.",
             rest: false,
             type: ArgType.String,
-            required: true
-        }
+            required: true,
+        },
     ],
     execute(ctx, [canvas]) {
         const targetCanvas = ForgeCanvas.canvases[canvas];
-        
+
         if (!targetCanvas || !(targetCanvas instanceof CanvasBuilder)) {
             return ctx.error("No canvas with the provided name.");
         }
 
         const canvasCtx = targetCanvas.getContext();
-        const { width, height } = canvasCtx.canvas;
+        const width = canvasCtx.canvas.width;
+        const height = canvasCtx.canvas.height;
 
         // Draw the full coordinates on the canvas
-        const coordText = `X: 0 - ${width}, Y: 0 - ${height}`;
-        targetCanvas.fillText(coordText, 10, height - 10, "16px Arial", 0xFFFFFF);
+        targetCanvas.fillText(`X: 0 - ${width}`, 10, height - 30, "16px Arial", 0xFFFFFF);
+        targetCanvas.fillText(`Y: 0 - ${height}`, 10, height - 10, "16px Arial", 0xFFFFFF);
 
-        return this.success();
-    }
+        // Render and create an attachment
+        const buffer = targetCanvas.render();
+        const attachment = new AttachmentBuilder(buffer, { name: `${canvas}.png` });
+
+        return this.success(attachment);
+    },
 });
